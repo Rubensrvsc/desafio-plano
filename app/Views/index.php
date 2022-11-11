8fc44767d5e9b757146049e79dcaf2746f60d6e4
rubensrvsc <rubensspfc100@gmail.com>
2022-11-11 11:59:29 -0300

    style: refactor style of index page

--- app/Views/index.php
+++ app/Views/index.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/index.css">
     <title>Document</title>
 </head>
 <body>
@@ -20,9 +22,42 @@
     }
     ?>
 
-    <?php foreach($listOrders as $order): ?>
-        <p>Valor: <?= $order['value'] ?></p>
-        <p>Descrição: <?= $order['description'] ?></p>
-    <?php endforeach; ?>
+
+    <div class="h2">
+    <h2>Lista de Ordens</h2>
+  </div>
+  <div class="table_section">
+    <table>
+      <thead>
+        <tr>
+          <th>Descrição</th>
+          <th>Valor</th>
+          <th>Cnpj</th>
+          <th>action</th>
+        </tr>
+      </thead>
+      <?php foreach($listOrders as $order): ?>
+      <tbody>
+        <tr>
+          <td><?= $order['description'] ?></td>
+          <td><?= $order['value'] ?></td>
+          <td><?= $order['cnpj'] ?></td>
+          <td>
+            <button>
+              <a href="<?php echo base_url('order') ?>" class="fa-solid fa-plus"></a>
+            </button>
+            <button>
+              <a href="<?php echo base_url('order/edit/'.$order['id']) ?>" class="fa-solid fa-pen-to-square"></a>
+            </button>
+            <button>
+              <a  onclick="return confirm('Deseja deletar essa ordem')" 
+              href="<?php echo base_url('order/delete/'.$order['id']) ?>" class="fa-solid fa-trash"></a>
+            </button>
+          </td>
+        </tr>
+      </tbody>
+      <?php endforeach; ?>
+    </table>
+  </div>
 </body>
 </html>
